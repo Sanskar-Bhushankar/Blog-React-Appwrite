c9a2bc3e1176899009d1ec99b22f944f7088b13a
Sanskar-Bhushankar <sanskarbhushankar01@gmail.com>
2025-06-08 17:49:15 +0530

    icon

--- react-blog/src/components/Logo.jsx
+++ react-blog/src/components/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import blogicon from '../assets/blog-icon.png'
 
 
 const Logo = () => {
   return (
     <div>
-      <img src="./src/assets/blog-icon.png"
+      <img src={blogicon}
       alt="logo"
       className='w-10' />
     </div>
